0ea7bd9fd478fc440655eb3e87edbce75cd7ab0e
Jonathan Park <joohopark27@gmail.com>
2018-11-05 22:32:21 -0500

    minor change

--- src/Mixer/Randomizer.java
+++ src/Mixer/Randomizer.java
@@ -34,13 +34,13 @@ public class Randomizer {
 
     private void randomize(){
 
-        int[] temp = array.clone();
-
         for(int i = array.length - 1; i >= 0; i--){
 
             int randomNum = (int) Math.floor(Math.random() * i);
-            array[i] = temp[randomNum];
-            temp[randomNum] = temp[i];
+
+            int temp = array[i];
+            array[i] = array[randomNum];
+            array[randomNum] = temp;
 
         }
 
