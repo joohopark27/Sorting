718ce97746cae124c2c8381d58bfd5aafc6d5b47
JonathanPark <joohopark27@gmail.com>
2019-01-10 08:31:24 -0500

    update

--- src/Tester.java
+++ src/Tester.java
@@ -5,18 +5,18 @@ public class Tester {
 
     public static void main(String[] args) {
 
-        Randomizer randomizer = new Randomizer(10);
+        Randomizer randomizer = new Randomizer(99999);
 
         int[] array = randomizer.getRandomArray();
 //        int[] array = {5, 4, 3, 2, 1};
 
 //        new BubbleSort(array.clone());
-//        new CombSort(array.clone());
+        new CombSort(array.clone());
 //        new BogoSort(array.clone());
 //        new SelectionSort(array.clone());
 //        new InsertionSort(array.clone());
-//        new MergeSort(array.clone());
-//        new RadixSort(array.clone());
+        new MergeSort(array.clone());
+        new RadixSort(array.clone());
         new QuickSort(array.clone());
 
     }
