d3c7b354a833b72020f5438c5dbebe4072e762ed
JonathanPark <joohopark27@gmail.com>
2019-03-01 07:46:33 -0500

    added heapsort

--- src/Tester.java
+++ src/Tester.java
@@ -5,19 +5,20 @@ public class Tester {
 
     public static void main(String[] args) {
 
-        Randomizer randomizer = new Randomizer(99999);
+        Randomizer randomizer = new Randomizer(10000);
 
         int[] array = randomizer.getRandomArray();
 //        int[] array = {5, 4, 3, 2, 1};
 
 //        new BubbleSort(array.clone());
-        new CombSort(array.clone());
+//        new CombSort(array.clone());
 //        new BogoSort(array.clone());
 //        new SelectionSort(array.clone());
 //        new InsertionSort(array.clone());
-        new MergeSort(array.clone());
-        new RadixSort(array.clone());
-        new QuickSort(array.clone());
+//        new MergeSort(array.clone());
+//        new RadixSort(array.clone());
+//        new QuickSort(array.clone());
+        new HeapSort(array.clone());
 
     }
 
